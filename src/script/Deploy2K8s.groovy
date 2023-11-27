d8a90d356c9a3e1ef9d058736d7c7371e28a5f41
jianghe <jianghe>
2023-11-27 11:36:27 +0800

    test

--- src/script/Deploy2K8s.groovy
+++ src/script/Deploy2K8s.groovy
@@ -6,5 +6,13 @@ def call(body) {
     def jobName = env.JOB_NAME
 
     echo "jobName: ${jobName}"
+
+
+    // 应用环境
+    def branch = env.BRANCH_NAME.replace('branches/','')
+    if (branch == null || branch == '') {
+        throw new RuntimeException("branch is null..")
+    }
+    echo "branch: ${branch}"
     echo 'Deploy2K8s'
 }
