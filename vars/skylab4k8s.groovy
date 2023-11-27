291c69881a3c1b09c8011362e11c98749cc1513e
jianghe <jianghe>
2023-11-27 10:34:53 +0800

    test

--- vars/skylab4k8s.groovy
+++ vars/skylab4k8s.groovy
@@ -3,13 +3,13 @@
 import script.Deploy2K8s
 
 def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-    echo 'hello ${config.name}'
+//    def config = [:]
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = config
+//    body()
+//
+//    echo 'hello ${config.name}'
 
     echo 'hello skylab4k8s'
-    new Deploy2K8s().call(){}
+//    new Deploy2K8s().call(){}
 }
